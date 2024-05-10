c795393752c38133de27fa2966cd570c3b20e981
Mateus Rodrigues <mateus.fr26@gmail.com>
2024-05-09 22:27:55 -0300

    Update bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -1,21 +1,25 @@
 # STEP 1: build universal binary
 
 swift package clean
-#swift build --product PluginExecutable -c release --arch arm64 --arch x86_64
-swift build --product PluginExecutable -c release
+
+# swift build --product SwiftUILintExecutable -c release --arch arm64 --arch x86_64
+
+# swift build --product SwiftUILintExecutable -c release
+
 rm -rf executable.artifactbundle && mkdir -p executable.artifactbundle/executable-macos/bin
-cp $(swift build --product PluginExecutable -c release --arch arm64 --arch x86_64 --show-bin-path)/PluginExecutable executable.artifactbundle/executable-macos/bin/PluginExecutable
+
+cp $(swift build --product SwiftUILintExecutable -c release --arch arm64 --arch x86_64 --show-bin-path)/SwiftUILintExecutable executable.artifactbundle/executable-macos/bin/SwiftUILintExecutable
 
 cat <<EOF > executable.artifactbundle/info.json
 {
   "schemaVersion": "1.0",
   "artifacts": {
-    "mycommand": {
+    "swiftuilint": {
       "version": "0.0.1",
       "type": "executable",
       "variants": [
         {
-          "path": "executable-macos/bin/PluginExecutable",
+          "path": "executable-macos/bin/SwiftUILintExecutable",
           "supportedTriples": [
             "x86_64-apple-macosx",
             "arm64-apple-macosx"
@@ -30,10 +34,14 @@ EOF
 # STEP 3: create artifact bundle zip
 
 cd executable.artifactbundle
+
 zip -r ../executable.artifactbundle.zip . -x '**/.*' -x '**/__MACOSX'
+
 #cd ..
+
 #rm -rf artifact
 
 readonly artifactbundle="executable.artifactbundle.zip"
+
 readonly checksum="$(shasum -a 256 "$artifactbundle" | cut -d " " -f1 | xargs)"
 
